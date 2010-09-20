e9474eb6cf1d72eece9a5ee8d0500a9177501233
Marcelo Toledo <marcelo@marcelotoledo.com>
2010-09-20 11:27:30 -0300

    Forgot to add misc.rb.

---
+++ trunk/misc.rb
@@ -0,0 +1,20 @@
+# misc.rb --- misc functions
+
+# Copyright  (C)  2010  Marcelo Toledo <marcelo@marcelotoledo.com>
+
+# Version: 1.0
+# Keywords: 
+# Author: Marcelo Toledo <marcelo@marcelotoledo.com>
+# Maintainer: Marcelo Toledo <marcelo@marcelotoledo.com>
+# URL: http://
+
+# Commentary: 
+
+
+
+# Code:
+require 'api_consts.rb'
+
+def pdebug(str)
+  print str if DEBUG == true
+end
