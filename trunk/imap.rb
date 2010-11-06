a984a1cad70bcd8af397df77966b21959b255b10
Eduardo Sampaio <esampaio@gmail.com>
2010-11-06 19:37:08 -0200

    Started converting the IMAP file into a class to move into the rails lib directory.

--- trunk/imap.rb
+++ trunk/imap.rb
@@ -13,63 +13,75 @@
 
 
 # Code:
-
 require 'net/imap'
 require 'rubygems'
 require 'yaml'
 require 'tmail'
 
-def get_url(body)
-  array = body.scan(/<a href="http:\/\/tira-teima\.as\.uol\.com\.br\/challengeSender\.html\?data=.*?" target=/)
-  array.empty? ? nil : array[0][9..-10]
-end
+class AntiSpamImapCollector
 
-# Source server connection info.
-MAIL_NAME = 'imap.vexcorp.com'
-MAIL_HOST = 'imap.vexcorp.com'
-MAIL_PORT = 143
-MAIL_SSL  = false
-MAIL_USER = 'antispamuol@vexcorp.com'
-MAIL_PASS = 'pee0theX'
+  # Source server connection info.
+  MAIL_NAME = 'imap.vexcorp.com'
+  MAIL_HOST = 'imap.vexcorp.com'
+  MAIL_PORT = 143
+  MAIL_SSL  = false
+  MAIL_USER = 'antispamuol@vexcorp.com'
+  MAIL_PASS = 'pee0theX'
 
-# MAIL_NAME = 'imap.gmail.com'
-# MAIL_HOST = 'imap.gmail.com'
-# MAIL_PORT = 993
-# MAIL_SSL  = true
-# MAIL_USER = 'xxx@gmail.com'
-# MAIL_PASS = 'xxxyyyzzz'
+  # MAIL_NAME = 'imap.gmail.com'
+  # MAIL_HOST = 'imap.gmail.com'
+  # MAIL_PORT = 993
+  # MAIL_SSL  = true
+  # MAIL_USER = 'xxx@gmail.com'
+  # MAIL_PASS = 'xxxyyyzzz'
 
-puts "Connecting..."
-m = Net::IMAP.new(MAIL_HOST, MAIL_PORT, MAIL_SSL)
+  def initialize
+    @m = Net::IMAP.new(MAIL_HOST, MAIL_PORT, MAIL_SSL)
+    @m.login(MAIL_USER, MAIL_PASS)
+    @m.select('INBOX')    
+  rescue
+    p 'Could not connect to IMAP Inbox: ' + $!
+    exit
+  end
+    
+  def populate
+    @m.search(["ALL"]).each do |msg_id|
+      
+      msg = @m.fetch(msg_id,'RFC822')[0].attr['RFC822']  
+      mail = TMail::Mail.parse(msg)
 
-puts 'Logging in...'
-m.login(MAIL_USER, MAIL_PASS)
+      url = get_url(mail.body)  
 
-puts "Going for INBOX..."
-m.select('INBOX')
+      if !url
+        @m.store(msg_id, "+FLAGS", [:Deleted])
+        next
+      end
+  
+      p url  
+      p mail.subject
+      p mail.friendly_from
+      p mail.from
+      p mail.to
+      p mail.date
+      p mail.body
+      
+    end
 
-m.search(["ALL"]).each do |msg_id|
-  msg = m.fetch(msg_id,'RFC822')[0].attr['RFC822']  
-  mail = TMail::Mail.parse(msg)
+    puts "Expunging messages"
+    @m.expunge
 
-  url = get_url(mail.body)  
-  if !url
-    puts "It's not Tira-Teima, marking for delete."
-    m.store(msg_id, "+FLAGS", [:Deleted])
-    next
+    puts "Disconnecting"
+    @m.disconnect
+  rescue
+    p 'An error ocurred while populating the database: ' + $!
   end
   
-  puts url  
-  p mail.subject
-  p mail.friendly_from
-  p mail.from
-  p mail.to
-  p mail.date
-  p mail.body
+  private
+  def get_url(body)
+    array = body.scan(/<a href="http:\/\/tira-teima\.as\.uol\.com\.br\/challengeSender\.html\?data=.*?" target=/)
+    array.empty? ? nil : array[0][9..-10]
+  end
+
 end
 
-puts "Expunging messages"
-m.expunge
-
-puts "Disconnecting"
-m.disconnect
+a = AntiSpamImapCollector.new.populate
